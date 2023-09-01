2b703778ca336e90ab8ec43b82f0f53ca3655d77
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-09-01 14:40:36 -0600

    Update TempConverter.java

--- src/TempConverter.java
+++ src/TempConverter.java
@@ -1,5 +1,18 @@
 public class TempConverter {
     public static void main(String[] args) {
-        
+        //promt user to tell if the temp they wish to convert is F or C or K
+        String Type = ????;
+        //Ensure user enters valid prompt
+        if Type does not equal;
+        //prompt user to input a tempetature
+        double InitialTemp = ????;
+        //convert C
+        if Type = C;
+        //Convert F
+        else if Type = F;
+        //Convert K
+        else if Type = K{
+
+        }
     }
 }
