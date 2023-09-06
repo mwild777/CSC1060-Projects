0d1df3d96737018b8183779e53ca4fa46cd8909e
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-09-06 17:47:06 -0600

    Create InteractiveGreeter.java

---
+++ src/InteractiveGreeter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class InteractiveGreeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        System.out.println("Where are you from? ");
+        System.out.println("How old are you (years)? ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
