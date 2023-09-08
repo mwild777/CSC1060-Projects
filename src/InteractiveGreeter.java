de8dd33dcfbdd56b056a45a9a67bb0e9b71f6d4f
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-09-08 17:13:59 -0600

    Update InteractiveGreeter.java

--- src/InteractiveGreeter.java
+++ src/InteractiveGreeter.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 public class InteractiveGreeter {
     public static void main(String[] args) {
+        //Create Scanner
         Scanner sc = new Scanner(System.in);
+        //ask for name
         System.out.println("What is your name? ");
+        //save name
+        String name = sc.nextLine();
+        //ask where they are from
         System.out.println("Where are you from? ");
+        //save where they are from
+        String from = sc.nextLine();
+        //ask for age
         System.out.println("How old are you (years)? ");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        //save age
+        String age = sc.nextLine();
+        //give back information
+        System.out.println("Hello " + name + " from " + from + ". You are " + age + " years old.");
+        //ask for hobby
+        System.out.println("Whats something you like to do? ");
+        //save hobby
+        String hobby = sc.nextLine();
+        //give back hobby
+        System.out.println("Have fun next time you " + hobby + "! ");
+        //ask for donation amount
+        System.out.println("How much would you like to donate today? ");
+        //save donation
+        String money = sc.nextLine();
+        //give back receipt. 
+        System.out.println("You will donate $" + money + " today. Yay!");
     }
 }
