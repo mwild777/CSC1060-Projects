f6017ed9abff3bbda7df700073ebb3c4860add74
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-09-13 17:54:56 -0600

    Update Main.java
    notes 9/13/23

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ public class Main {
         String yolo = "You only live once!";
         int SmallNumber = 15;
         int BigNumber = 1500;
-        System.out.println(SmallNumber *BigNumber);
+        System.out.println(SmallNumber * BigNumber);
         System.out.println(BigNumber / SmallNumber);
         System.out.println(yolo);
-        System.out.println("The homie to my left has a for loop going on!");
         System.out.println("Help! I need somebody!");
         System.out.println("Help! Not just anybody!");
         System.out.println("You know I need someone to help.");
@@ -14,3 +13,29 @@ public class Main {
         System.out.println("I never needed anybody's help in any way.");
     }
 }
+/*
+relationship operators
+    == equals (checks memory adress)
+    != does not equal
+    > lesser than
+    >= lesser or equal
+    < greater than
+    <= greater or equal
+    boolean true or false
+
+condition statements
+    if
+    else
+    else if
+
+logical operators
+    || or
+    && and
+    ! not
+
+compare stings
+    *firstString*.equals(*secondString*)
+
+
+
+*/
