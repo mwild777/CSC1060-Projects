ebc76fc74d06de560fffa09c5c1ef8de01f6247e
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-09-26 20:21:11 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String yolo = "You only live once!";
-        int SmallNumber = 15;
-        int BigNumber = 1500;
-        System.out.println(SmallNumber * BigNumber);
-        System.out.println(BigNumber / SmallNumber);
-        System.out.println(yolo);
-        System.out.println("Help! I need somebody!");
-        System.out.println("Help! Not just anybody!");
-        System.out.println("You know I need someone to help.");
-        System.out.println("When I was younger, so much younger than today.");
-        System.out.println("I never needed anybody's help in any way.");
+
+        String fruit = "banana";
+        fruit.substring(0);
     }
 }
 /*
@@ -35,7 +27,4 @@ logical operators
 
 compare stings
     *firstString*.equals(*secondString*)
-
-
-
 */
