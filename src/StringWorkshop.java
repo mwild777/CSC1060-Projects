0a00262bc1fec9894bed9717b4d8ce0be7455f07
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-10-02 20:41:39 -0600

    Update StringWorkshop.java
    add comments

--- src/StringWorkshop.java
+++ src/StringWorkshop.java
@@ -31,6 +31,7 @@ public class StringWorkshop {
         }
     }
     public static String menuFilter(String string, int choice) {
+        //send to correct choice
         if (choice == 1) string = one(string);
         else if (choice == 2) two(string);
         else if (choice == 3) three(string);
@@ -42,49 +43,75 @@ public class StringWorkshop {
     public static String one(String string) {
         //make scanner
         Scanner sc = new Scanner(System.in);
+        //store string
         String addMe = sc.nextLine();
+        //add strings
         string = string + addMe;
+        //print new string
         System.out.println(string);
+        //return final string
         return string;
     }
     public static void two(String string) {
         //make scanner
         Scanner sc = new Scanner(System.in);
+        //ask for substring
         System.out.println("Substring: ");
+        //store substring
         String substring = sc.nextLine();
+        //print if substring exists in string
         System.out.println(string.contains(substring));
     }
     public static void three(String string) {
+        //create int length
         int length = string.length();
+        //print length
         System.out.println(length);
     }
     public static void four(String string) {
         //make scanner
         Scanner sc = new Scanner(System.in);
+        //ask for prefix
         System.out.println("Prefix: ");
+        //store prefix
         String prefix = sc.nextLine();
+        //measure prefix
         int length = prefix.length();
+        //find where to start printing
         int preIndex = string.indexOf(prefix) + length;
+        //print
         System.out.println(string.substring(preIndex));
     }
     public static void five(String string) {
         //make scanner
         Scanner sc = new Scanner(System.in);
+        //ask for suffix
         System.out.println("Suffix: ");
+        //store suffix
         String suffix = sc.nextLine();
+        //find where the suffix starts
         int sufIndex = string.indexOf(suffix);
+        //print from the start of the string to the suffix
         System.out.println((string.substring(0, sufIndex)));
     }
     public static void six(String string) {
         //make scanner
         Scanner sc = new Scanner(System.in);
+        //ask for prefix
         System.out.println("Prefix: ");
+        //store prefix
         String prefix = sc.nextLine();
+        //ask for suffix
         System.out.println("Suffix: ");
+        //store suffix
         String suffix = sc.nextLine();
+        //get the length of the prefix
         int length = prefix.length();
+        //find the end of the prefix
         int preIndex = string.indexOf(prefix) + length;
+        //find the start of the suffix
         int sufIndex = string.indexOf(suffix);
+        //print from the end of the prefix to the start of the suffix
         System.out.println(string.substring(preIndex, sufIndex));
     }
 }
